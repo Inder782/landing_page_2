f5ffda13d4597e43b35b2bef856e87887917a753
inder782 <inderchandel782@gmail.com>
2024-03-25 17:52:11 +0530

    feat(section_2):responsive section 2 v1.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero/Hero";
+import Section_2 from "@/components/section_2/Section_2";
 import React from "react";
 
 const page = () => {
   return (
     <div>
       <Hero />
+      <Section_2 />
     </div>
   );
 };
