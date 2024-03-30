e84e301a4e61f768a8f4802f880c5055c5ef5557
inder782 <inderchandel782@gmail.com>
2024-03-30 18:57:57 +0530

    feat(section_6):initial layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Section_2 from "@/components/section_2/Section_2";
 import Section_3 from "@/components/section_3/Section_3";
 import Section_4 from "@/components/section_4/Section_4";
 import Section_5 from "@/components/section_5/Section_5";
+import Section_6 from "@/components/section_6/Section_6";
 import React from "react";
 
 const page = () => {
@@ -13,6 +14,7 @@ const page = () => {
       <Section_3 />
       <Section_4 />
       <Section_5 />
+      <Section_6 />
     </div>
   );
 };
